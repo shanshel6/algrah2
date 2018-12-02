d5ceb7c820a78221a39279b7d79b84ef8a8036de
Hassan <shanshel6@gmail.com>
2018-12-02 15:40:51 +0300

    the last commit

--- algrah/settings.py
+++ algrah/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'items.apps.ItemsConfig',
     'sections.apps.SectionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- items/models.py
+++ items/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    image = image = models.ImageField(upload_to='images/')
+    desc = models.TextField()
