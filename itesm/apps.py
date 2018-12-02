70abcecf8a0b33c5bbf0e6adcc4f937840ca16bc
Hassan <shanshel6@gmail.com>
2018-12-02 12:24:54 +0300

    third commit

---
+++ itesm/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ItesmConfig(AppConfig):
+    name = 'itesm'
---
+++ sections/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=60)
